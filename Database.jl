5c35094a6234a7f8c0d9c2a1a86f4b26095da94e
Kushan Singh <kushansingh22@gmail.com>
2019-05-03 16:39:20 -0400

    Database Added

--- Database.jl
+++ Database.jl
@@ -3,11 +3,11 @@ module Database
 using MySQL
 
 const HOST = "localhost"
-const USER = "root"
-const PASSWORD = "Coldplay1"
+const USER = ""
+const PASSWORD = ""
 const DB = "six-degrees"
-const PORT = 8889
-const SOCKET = "/Applications/MAMP/tmp/mysql/mysql.sock"
+const PORT = 
+const SOCKET = ""
 
 const CONN = MySQL.connect(HOST,USER,PASSWORD,db=DB,port=PORT,unix_socket=SOCKET)
 
